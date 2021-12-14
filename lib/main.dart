5efd9cf6018541e233a1433c127638682850b8b9
furki911 <bhatfurqan@gmail.com>
2021-12-14 21:54:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+import './quiz.dart';
+import './result.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  final _questions = const [
+    {
+      'questionText': 'what\'s your favourite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'white', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'what\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Tiger', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ]
+    },
+    {
+      'questionText': 'who\'s your favourite instructor?',
+      'answers': [
+        {'text': 'Ali', 'score': 1},
+        {'text': 'Baba', 'score': 1},
+        {'text': 'Chalis', 'score': 1},
+        {'text': 'Choar', 'score': 1}
+      ]
+    }
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
+  void _resetQuiz() {
+    setState() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hola Amigo"),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
